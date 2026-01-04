684e9331b02efede8d0fb6a92f177046b5fe6534
KenWill24 <lwilliams@hylamobile.com>
2026-01-04 01:26:38 -0600

    updated layout.pug template

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,13 @@ var express = require('express');
 var router = express.Router();
 
 // Home route
+// Home route
 router.get('/', (req, res) => {
-  res.render('index', { title: 'Library Manager' });
+  res.render('index', { 
+    title: 'Library Manager',
+    showHomeLink: true
+  });
 });
+
 
 module.exports = router;
